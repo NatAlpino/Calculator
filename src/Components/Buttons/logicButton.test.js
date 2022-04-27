f5fae86694ec7f52bd13cdd2c46977b24e7d6ff5
NatAlpino <nathyalpino@hotmail.com>
2022-04-27 17:14:30 -0300

    tests

---
+++ src/Components/Buttons/logicButton.test.js
@@ -0,0 +1,61 @@
+import {
+  porcentage,
+  clear,
+  backspace,
+  positNegative,
+  inputNum,
+  operatorHandler,
+  results,
+} from "./logicButton";
+
+describe("Porcentage", () => {
+  test("extrai a porcentagem do numero indicado na tela de input", () => {
+    expect(porcentage);
+  });
+});
+
+describe("Clear", () => {
+  test("deve limpar a tela de input sempre que o botao clear for acionado", () => {
+    expect(clear);
+  });
+});
+
+describe("Backspace", () => {
+  test("deve zerar qualquer operacao para que uma nova seja iniciada", () => {
+    expect(backspace);
+  });
+});
+
+describe("PositNegative", () => {
+  test("transforma um numero positivo em negativo e vice versa", () => {
+    expect(positNegative);
+  });
+});
+
+describe("InputNum", () => {
+  test("carrega na tela de input o numero selecionado", () => {
+    expect(inputNum);
+  });
+});
+
+describe("OperatorHandler", () => {
+  test("faz a divisao dos numeros selecionados", () => {
+    expect(operatorHandler);
+  });
+  test("faz a multiplicacao dos numeros selecionados", () => {
+    expect(operatorHandler);
+  });
+  test("faz a soma dos numeros selecionados", () => {
+    expect(operatorHandler);
+  });
+  test("faz a subtracao dos numeros selecionados", () => {
+    expect(operatorHandler);
+  });
+});
+
+describe("Results", () => {
+  test("apresenta na tela de input, o resultado da operacao anterior", () => {
+    expect(results);
+  });
+});
+
